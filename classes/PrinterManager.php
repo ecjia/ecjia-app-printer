5bf7b53f62664f3dedaa8ccb3a6eda962df8b2c2
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-12-13 11:34:56 +0800

    添加打印管理类

---
+++ classes/PrinterManager.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Ecjia\App\Printer;
+
+use ecjia_config;
+
+class PrinterManager
+{
+    private $appKey;
+    private $appSecret;
+    
+    protected $printer;
+    
+    public function __construct($appKey = null, $appSecret = null)
+    {
+        $this->appKey = $appKey ?: ecjia_config::get('printer_key');
+        $this->appSecret = $appSecret ?: ecjia_config::get('printer_secret');
+        $this->printer = royalcms('printer');
+        
+        $config = [
+            'app_key' => $this->appKey,
+            'app_secret' => $this->appSecret,
+        ];
+        $this->printer->configure($config);
+    }
+    
+    
+    /**
+     * 设置打印回调地址
+     */
+    public function setNotify($finish = null, $getOrder = null, $printStatus = null)
+    {
+        $oauth_finish = $finish ?: ecjia_config::get('printer_print_push');
+        $oauth_getOrder = $getOrder ?: ecjia_config::get('printer_order_push');
+        $oauth_printStatus = $printStatus ?: ecjia_config::get('printer_status_push');
+        
+        $resp = $this->printer->request('yly/printer/setnotify', function ($req) use ($oauth_finish, $oauth_getOrder, $oauth_printStatus) {
+        	$req->setOauthFinish($oauth_finish);
+        	$req->setOauthGetOrder($oauth_getOrder);
+        	$req->setOauthPrintStatus($oauth_printStatus);
+        });
+        
+        dd($resp);
+    }
+    
+    
+    
+}
