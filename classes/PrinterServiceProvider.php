cc1b7c6412f2b2be0a03596f55eab802ab57c0fe
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-30 23:36:01 +0800

    指定ServiceProvider的路径

--- classes/PrinterServiceProvider.php
+++ classes/PrinterServiceProvider.php
@@ -57,7 +57,7 @@ class PrinterServiceProvider extends AppParentServiceProvider
      */
     public function boot() 
     {
-        $this->package('ecjia/app-printer');
+        $this->package('ecjia/app-printer', null, dirname(__DIR__));
     }
 
 	/**
